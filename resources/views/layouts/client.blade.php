c84356f3b032ebf216bd9d702edc011fe8eb5d18
Nguyen Dat <datnv1997@gmail.com>
2019-11-30 17:18:18 +0700

    udpate

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -32,9 +32,32 @@
     <!--[if lt IE 9]><script type="text/javascript" src="js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="{{ asset('client/js/ie-emulation-modes-warning.js') }}"></script>
 
+    <style>
+    .footer-info .main-title-2 h1 {
+        color: white !important;
+    }
+
+    footer .popular-posts .media-heading a {
+        color: black !important;
+    }
+
+    .media-left {
+        vertical-align: middle !important;
+    }
+
+    footer ul li,
+    footer ul li a,
+    footer p {
+        color: black !important;
+    }
+    </style>
 
     <!-- Scripts -->
-    <script>window.Laravel = {!! json_encode(['csrfToken' => csrf_token(),]) !!};</script>
+    <script>
+    window.Laravel = {
+        !!json_encode(['csrfToken' => csrf_token(), ]) !!
+    };
+    </script>
 </head>
 
 <body>
@@ -48,7 +71,8 @@
                         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                             <div class="list-inline">
                                 <a href="tel_3A1-8X0-666-8X88"><i class="fa fa-phone"></i>0977 915 883</a>
-                                <a href="tel_3Ainfo_40themevessel.com"><i class="fa fa-envelope"></i>vanthanh@gmail.com</a>
+                                <a href="tel_3Ainfo_40themevessel.com"><i
+                                        class="fa fa-envelope"></i>vanthanh@gmail.com</a>
                             </div>
                         </div>
                         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
@@ -71,7 +95,8 @@
                 <div class="container">
                     <nav class="navbar navbar-default">
                         <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navigation" aria-expanded="false">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                data-target="#app-navigation" aria-expanded="false">
                                 <span class="sr-only">Toggle navigation</span>
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
@@ -82,19 +107,23 @@
                             </a>
                         </div>
                         <!-- Collect the nav links, forms, and other content for toggling -->
-                        <div class="navbar-collapse collapse" role="navigation" aria-expanded="true" id="app-navigation">
+                        <div class="navbar-collapse collapse" role="navigation" aria-expanded="true"
+                            id="app-navigation">
                             <ul class="nav navbar-nav">
                                 <li class="dropdown active">
-                                    <a tabindex="0" data-toggle="dropdown" data-submenu="" aria-expanded="false">Trang chủ</a>
+                                    <a tabindex="0" data-toggle="dropdown" data-submenu="" aria-expanded="false">Trang
+                                        chủ</a>
                                 </li>
                                 <li class="dropdown mega-dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Giới thiệu</a>
                                 </li>
                                 <li class="dropdown">
-                                    <a tabindex="0" data-toggle="dropdown" data-submenu="" aria-expanded="false">Chuyên khoa</a>
+                                    <a tabindex="0" data-toggle="dropdown" data-submenu="" aria-expanded="false">Chuyên
+                                        khoa</a>
                                 </li>
                                 <li class="dropdown">
-                                    <a tabindex="0" data-toggle="dropdown" data-submenu="" aria-expanded="false">Lịch làm việc bác sĩ</a>
+                                    <a tabindex="0" data-toggle="dropdown" data-submenu="" aria-expanded="false">Lịch
+                                        làm việc bác sĩ</a>
                                 </li>
                                 <li class="dropdown mega-dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Liên hệ</a>
@@ -116,145 +145,77 @@
 
 
             <!-- Footer start -->
-            <footer class="main-footer clearfix">
+            <footer class="main-footer clearfix top-header">
                 <div class="container">
                     <!-- Footer info-->
                     <div class="footer-info">
                         <div class="row">
                             <!-- About us -->
-                            <div class="col-lg-4 col-md-3 col-sm-6 col-xs-12">
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                                 <div class="footer-item">
                                     <div class="main-title-2">
-                                        <h1>Contact Us</h1>
+                                        <h1>Liên hệ</h1>
                                     </div>
-                                    <p>
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's printing and
-                                    </p>
+
                                     <ul class="personal-info">
                                         <li>
                                             <i class="fa fa-map-marker"></i>
-                                            Address: 20/F Green Road, Dhanmondi, Dhaka
+                                            Địa chỉ: 175 Tây Sơn, Đống Đa, Hà Nội
                                         </li>
                                         <li>
                                             <i class="fa fa-envelope"></i>
-                                            Email:<a href="mailto:sales@hotelempire.com">info@themevessel.com</a>
+                                            Email:<a href="mailto:sales@hotelempire.com">datnv1997@gmail.com</a>
                                         </li>
                                         <li>
                                             <i class="fa fa-phone"></i>
-                                            Phone: <a href="tel_3A+55-417-634-7071">+55 4XX-634-7071</a>
+                                            Số điện thoại: <a href="tel_0359501670">0359501670</a>
                                         </li>
-                                        <li>
-                                            <i class="fa fa-fax"></i>
-                                            Fax: +55 4XX-634-7071
-                                        </li>
+
                                     </ul>
                                 </div>
                             </div>
-                            <!-- Links -->
-                            <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
-                                <div class="footer-item">
-                                    <div class="main-title-2">
-                                        <h1>Links</h1>
-                                    </div>
-                                    <ul class="links">
-                                        <li>
-                                            <a href="index.html">Home</a>
-                                        </li>
-                                        <li>
-                                            <a href="about.html">About Us</a>
-                                        </li>
-                                        <li>
-                                            <a href="contact.html">Contact Us</a>
-                                        </li>
-                                        <li>
-                                            <a href="blog-single-sidebar-right.html">Blog</a>
-                                        </li>
-                                        <li>
-                                            <a href="blog-single-sidebar-right.html">Services</a>
-                                        </li>
-                                        <li>
-                                            <a href="properties-list-rightside.html">properties Listing</a>
-                                        </li>
-                                        <li>
-                                            <a href="properties-grid-rightside.html">properties Grid</a>
-                                        </li>
-                                        <li>
-                                            <a href="properties-details.html">properties Details</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+
                             <!-- Recent cars -->
-                            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                                 <div class="footer-item popular-posts">
                                     <div class="main-title-2">
-                                        <h1>Popular Posts</h1>
+                                        <h1>Giờ làm việc</h1>
                                     </div>
                                     <div class="media">
-                                        <div class="media-left">
-                                            <img class="media-object" src="img/properties/small-properties-1.jpg" alt="small-properties-1">
+                                        <div class="media-left" style="width:40%">
+                                            <h3 class="media-heading">
+                                                <a href="properties-details.html">Từ thứ 2 - thứ 6:</a>
+                                            </h3>
                                         </div>
                                         <div class="media-body">
-                                            <h3 class="media-heading">
-                                                <a href="properties-details.html">Sweet Family Home</a>
-                                            </h3>
-                                            <p>February 27, 2018</p>
-                                            <div class="price">
-                                                $734,000
-                                            </div>
+                                            <p>Sáng: 7h30 - 12h30</p>
+                                            <p>Chiều: 1h30 - 18h30</p>
                                         </div>
                                     </div>
                                     <div class="media">
-                                        <div class="media-left">
-                                            <img class="media-object" src="img/properties/small-properties-2.jpg" alt="small-properties-2">
+                                        <div class="media-left" style="width:40%">
+                                            <h3 class="media-heading">
+                                                <a href="properties-details.html">Từ thứ 7 - chủ nhật:</a>
+                                            </h3>
                                         </div>
                                         <div class="media-body">
-                                            <h3 class="media-heading">
-                                                <a href="properties-details.html">Modern Family Home</a>
-                                            </h3>
-                                            <p>February 27, 2018</p>
-                                            <div class="price">
-                                                $734,000
-                                            </div>
+                                            <p>Sáng: 9h30 - 11h30</p>
+                                            <p>Chiều: 1h30 - 18h30</p>
                                         </div>
                                     </div>
-                                    <div class="media">
-                                        <div class="media-left">
-                                            <img class="media-object" src="img/properties/small-properties-3.jpg" alt="small-properties-3">
-                                        </div>
-                                        <div class="media-body">
-                                            <h3 class="media-heading">
-                                                <a href="properties-details.html">Beautiful Single Home</a>
-                                            </h3>
-                                            <p>February 27, 2018</p>
-                                            <div class="price">
-                                                $734,000
-                                            </div>
-                                        </div>
-                                    </div>
+
                                 </div>
                             </div>
                             <!-- Subscribe -->
-                            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                                 <div class="footer-item">
                                     <div class="main-title-2">
-                                        <h1>Subscribe</h1>
+                                        <h1>Phòng khám nhi Hồng Dương</h1>
                                     </div>
                                     <div class="newsletter clearfix">
-                                        <p>
-                                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                        </p>
+                                        <img src="/client/img/logos/logo.png">
 
-                                        <form action="#" method="post">
-                                            <div class="form-group">
-                                                <input class="nsu-field btn-block" id="nsu-email-0" type="text" name="email" placeholder="Enter your Email Address" required="">
-                                            </div>
-                                            <div class="form-group mb-0">
-                                                <button type="submit" class="button-sm button-theme btn-block">
-                                                    Subscribe
-                                                </button>
-                                            </div>
-                                        </form>
+
                                     </div>
                                 </div>
                             </div>
